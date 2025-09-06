80c5f368d63786d58bc69e56e92123196cc2391a
TishkaPlay <tix.tot@yandex.ru>
2025-09-06 13:13:29 +0300

    completed laba 1

--- Program.cs
+++ Program.cs
@@ -36,6 +36,62 @@ namespace Laba_One
             //Console.WriteLine(int.MinValue);
 
 
+            //Console.Write("Введите ваше имя: ");
+            //string? name = Console.ReadLine();
+            //Console.Write("Введите ваш возраст: ");
+            //int? age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Привет {name}!");
+            //Console.WriteLine($"Через год тебе будет {age+1}");
+
+            // Zadanie 1
+            //string? favoriteAnime = "One piece";
+            //int? favoriteDigital = 8;
+            //double PI = Math.PI;
+            //char favoriteChar = 'A';
+            //Console.WriteLine($"{favoriteAnime}, {favoriteDigital}, {PI}, {favoriteChar}");
+
+            //Zadanie 2
+            //Console.WriteLine("I");
+            //Console.WriteLine("need");
+            //Console.WriteLine("more");
+            //Console.WriteLine("power!");
+
+            // Zadanie 3
+            //Console.WriteLine(" \"Hello There\" ");
+
+            // Zadanie 4
+            //int monik = int.Parse(Console.ReadLine());
+            //int sysblock = int.Parse(Console.ReadLine());
+            //int keyboard = int.Parse(Console.ReadLine());
+            //int mouse = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{(monik + sysblock + keyboard + mouse) * 3 }");
+
+            // Zadanie 5
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"{3 * Math.Pow((a + b), 3) + 275 * (b * b) - 127 * a - 41}");
+
+            //Zadanie 6
+            //Console.Write("Введите ваше имя: ");
+            //string? name = Console.ReadLine();
+            //Console.Write("Введите ваш возраст: ");
+            //int? age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Привет {name}!");
+            //Console.WriteLine($"Через год тебе будет {age+1}");
+
+            //Zadanie 7
+            //Console.Write("Введите ваше имя: ");
+            //string? name = Console.ReadLine();
+            //Console.Write("Введите ваш возраст: ");
+            //int? age = int.Parse(Console.ReadLine());
+            //Console.Write("Введите ваш город: ");
+            //string? city = Console.ReadLine();
+            //Console.WriteLine($"Привет {name}! Тебе {age} лет и ты живёшь в городе {city}.");
+
+            //Zadanie 8
+            //int? degrees = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{(degrees * 9 / 5) + 32} ");
         }
     }
 }

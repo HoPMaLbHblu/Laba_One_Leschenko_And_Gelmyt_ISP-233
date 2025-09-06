3352ad64da1662d930680fa3cbf8cb7014384500
TishkaPlay <tix.tot@yandex.ru>
2025-09-06 12:44:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Laba_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //int number = 10;
+            //Console.WriteLine(number);
+
+            //Console.Write("Введите ваше имя: ");
+            //string? name = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя: - {name}");
+
+            //string? name = Console.ReadLine();
+            //int? age = Convert.ToInt32(Console.ReadLine());
+            //int age2 = int.Parse(Console.ReadLine());
+            //string? city = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя - {name}, ваш возраст - {age}, ваш город - {city}");
+            //Console.WriteLine("{0}, {1}, {2}", name, age, city);
+
+            int number = 250;
+            byte number2 = (byte)(number+10);
+            Console.WriteLine(number2);
+            bool alive = false;
+            bool isDead = true; //я гуль;;;;;;;
+            double iDouble = 4.5;
+            float iFloat = 4.3f;
+            decimal iDecimal = 3.4m;
+            var a = 12;
+            System.Int32 n;
+
+            char symb = 'a';
+            Console.WriteLine(int.MinValue);
+
+
+        }
+    }
+}
+
